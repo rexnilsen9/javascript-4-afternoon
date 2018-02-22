326a79897c449b5f9496780d82aa69b6115167fa
rex nilsen <rexpnilsen9@gmail.com>
2018-02-22 14:56:21 -0700

    constructor commit

--- constructors.js
+++ constructors.js
@@ -14,7 +14,10 @@
 */
 
 // Code here
-
+function CarFactory(make, model){
+  this.make = make;
+  this.model = model;
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -34,6 +37,7 @@ function Employee(name, email, hireDate) {
 
 // Code here
 
+var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 
 
 ////////// PROBLEM 4 //////////
@@ -55,4 +59,13 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 
 // Code here
 
-
+function Car(make, model, year){
+  this.move = 0;
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  
+    this.moveCar = function(){
+      return this.move += 10
+    }
+  }
